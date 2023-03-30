5fd66e032920bbba755fa2156b28d7ca2d7f3564
Prateek Barve <prateekbarve2003@gmail.com>
2023-03-30 23:05:19 +0530

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,30 @@
+
+import ExpenseFilter from "./ExpenseFilter";
+import Card from '../UI/Card';
+import "./Expenses.css";
+import { useState } from 'react';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const getYearData = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
+    const filteredExpenses = props.items.filter((expense)=>{
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    return (
+        <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onYearSelection={getYearData}/>
+            <ExpensesChart expenses={filteredExpenses}/>
+            <ExpensesList items={filteredExpenses}/>
+        </Card>
+    )
+}
+
+export default Expenses; 
